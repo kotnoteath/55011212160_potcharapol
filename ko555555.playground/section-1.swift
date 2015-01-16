36054104763dac877e15a8c637859f0e23957edc
kotnoteath <kotnoteath@gmail.com>
2015-01-16 16:51:04 +0700

    new file

---
+++ ko555555.playground/section-1.swift
@@ -0,0 +1,39 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let a1 = 55;
+let a2 = 45;
+println(999999999999999);
+let r=random();
+if (r>0){
+println(888)}
+else {
+println(77777)}
+
+var arr = [String]()
+arr=["aaa","bbb"]
+
+var ara = ["ccc","ddd"]
+println(ara[0])
+
+
+var arr1 = ["a","b","c"]
+for (var i=0 ; i < arr1.count; i++){
+println(arr1[i])
+}
+
+var arr2 = ["x","y","z"]
+var str1 = ""
+for str1 in arr2 {
+println(str1)}
+
+
+
+
+
+
+
+
