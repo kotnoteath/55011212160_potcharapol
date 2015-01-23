a91d197be7196dd3481e57a5adcd44b7853a858c
kotnoteath <kotnoteath@gmail.com>
2015-01-23 17:01:04 +0700

    week2

--- username.playground/section-1.swift
+++ username.playground/section-1.swift
@@ -1,8 +1,31 @@
 // Playground - noun: a place where people can play
 
-var us:[String:String]
-var i=0
+var us:[String:String] = ["ant":"sugar","bat":"blood","cat":"fish","dragon":"human"]
+
+
+func add(){
 var uu="aaaa"
 var ss="0000"
+us.updateValue(ss, forKey: uu)//add
 
-us.updateValue("aaaa", forKey: "0000")
+}
+
+func up(){
+var uu="cat"
+us.updateValue("rat", forKey: uu)//update
+
+}
+
+func rem(){
+    var uu="ant"
+us.removeValueForKey(uu)//remove
+
+
+}
+add()
+println(us["aaaa"])
+up()
+println(us["cat"])
+rem()
+println(us["ant"])
+
